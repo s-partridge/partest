e86a9c488cd4f43e398d0d9ccbaf346c455d30ec
Samuel Partridge <s-partridge@hotmail.com>
2026-07-20 16:20:01 -0700

    Extracted EventDispatcherInterface, added SerialEventDispatcher

--- source/partestdispatcher.h
+++ source/partestdispatcher.h
@@ -15,7 +15,90 @@
 
 namespace partest
 {
-	class EventDispatcher {
+	class EventDispatcherInterface
+	{
+	protected:
+		// Owning queue of events. Each event is a pair of event type string and a pointer to an EventInterface object.
+		std::queue<EventPair> m_eventQueue;
+		// Non-owning vector of reporter interfaces. These are the subscribers that will receive events from the dispatcher.
+		std::vector<EventReporterInterface *> m_reporters;
+
+		std::atomic<bool> m_dispatching; // True until killDispatcher is called
+
+		EventPair popEventUnsafe() noexcept
+		{
+			EventPair eventPair = std::move(m_eventQueue.front());
+			m_eventQueue.pop();
+			return eventPair;
+		}
+
+	public:
+		EventDispatcherInterface(bool dispatching = true) : m_dispatching(dispatching) {}
+		virtual ~EventDispatcherInterface() = default;
+
+		// Delete copy and move constructors and assignment operators
+		EventDispatcherInterface(const EventDispatcherInterface&) = delete;
+		EventDispatcherInterface(EventDispatcherInterface&&) = delete;
+		EventDispatcherInterface& operator=(const EventDispatcherInterface&) = delete;
+		EventDispatcherInterface& operator=(EventDispatcherInterface&&) = delete;
+
+		bool isDispatching() const noexcept
+		{
+			return m_dispatching;
+		}
+
+		virtual void killDispatcher()
+		{
+			m_dispatching = false;
+			m_eventQueue.emplace(EVENT_DIE, std::make_unique<EventDie>(0, 0)); // Push an EventDie to signal the dispatcher to stop
+		}
+
+		virtual bool pushEvent(const std::string &eventType, std::unique_ptr<EventInterface> event) = 0;
+
+		virtual void registerReporter(EventReporterInterface *reporter) = 0;
+
+		virtual void dispatchEvents() = 0;
+	};
+
+	class SerialEventDispatcher : EventDispatcherInterface
+	{
+	public:
+		SerialEventDispatcher(bool dispatching = true) : EventDispatcherInterface(dispatching) {}
+		~SerialEventDispatcher() override = default;
+
+		bool pushEvent(const std::string &eventType, std::unique_ptr<EventInterface> event) override
+		{
+			if(!isDispatching())
+				return false;
+
+			m_eventQueue.emplace(eventType, std::move(event));
+			dispatchEvents();
+		}
+
+		void registerReporter(EventReporterInterface *reporter) override
+		{
+			m_reporters.push_back(reporter);
+		}
+
+		void dispatchEvents() override
+		{
+			while(!m_eventQueue.empty())
+			{
+				EventPair eventPair = popEventUnsafe();
+				// Get temporary copy of reporters, since a reporter could theoretically be added in response to an event and invalidate our iterator.
+				std::vector<EventReporterInterface *> localReporters = m_reporters;
+
+				// Dispatch the event to all registered reporters
+				for(EventReporterInterface *reporter : localReporters)
+				{
+					reporter->reportEvent(eventPair);				
+				}
+			}
+		}
+	};
+
+	class EventDispatcher
+	{
 		// Owning queue of events. Each event is a pair of event type string and a pointer to an EventInterface object.
 		std::queue<EventPair> m_eventQueue;
 		// Non-owning vector of reporter interfaces. These are the subscribers that will receive events from the dispatcher.
@@ -24,7 +107,14 @@ namespace partest
 		std::mutex m_reportersMutex;
 		counting_semaphore<> m_eventSemaphore;
 
-		std::atomic<bool>m_dispatching; // Flag to indicate whether the dispatcher is currently dispatching events
+		std::atomic<bool> m_dispatching; // True until killDispatcher is called
+
+		EventPair popEventUnsafe() noexcept
+		{
+			EventPair eventPair = std::move(m_eventQueue.front());
+			m_eventQueue.pop();
+			return eventPair;
+		}
 
 	public:
 		EventDispatcher() : m_dispatching(true) {};
@@ -52,13 +142,6 @@ namespace partest
 			return m_dispatching;
 		}
 
-		EventPair popEventUnsafe() noexcept
-		{
-			EventPair eventPair = std::move(m_eventQueue.front());
-			m_eventQueue.pop();
-			return eventPair;
-		}
-
 		void registerReporter(EventReporterInterface *reporter)
 		{
 			std::lock_guard<std::mutex> lock(m_reportersMutex);
@@ -78,7 +161,6 @@ namespace partest
 		*/
 		void dispatchEvents()
 		{
-			// DO ***NOT*** use AUTO for any variable
 			// Block until the queue is not empty. Use semaphore to wait for events to be pushed.
 			while(true)
 			{
