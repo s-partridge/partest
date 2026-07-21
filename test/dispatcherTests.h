454eb0a893386180fae8b900bc35f57cacea5ae9
Samuel Partridge <s-partridge@hotmail.com>
2026-07-20 20:52:32 -0700

    Added EventDispatcher tests stub, mock Logger class

---
+++ test/dispatcherTests.h
@@ -0,0 +1,126 @@
+#ifndef DISPATCHER_TESTS_H
+#define DISPATCHER_TESTS_H
+
+#include <memory>
+
+#include "partestdispatcher.h"
+#include "partestreporter.h"
+#include "partestbase.h"
+
+class MockReporter : public partest::EventReporterInterface
+{
+	std::vector<partest::EventPair> m_logs;
+
+	void writeLog(PARTEST_STRING_PARAM eventType, std::unique_ptr<partest::EventInterface> event)
+	{
+		m_logs.emplace_back(std::make_pair(eventType, std::move(event)));
+	}
+
+public:
+	MockReporter() = default;
+
+	void onTestBegin(const partest::EventBeginTest &event) override
+	{
+		writeLog(EVENT_BEGIN_TEST, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	void onTestEnd(const partest::EventEndTest &event) override
+	{
+		writeLog(EVENT_END_TEST, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	void onAssertion(const partest::EventAssertion &event) override
+	{
+		writeLog(EVENT_ASSERTION, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	void onLog(const partest::EventLog &event) override
+	{
+		writeLog(EVENT_LOG, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	void onPassthrough(const partest::EventPassthrough &event) override
+	{
+		writeLog(EVENT_PASSTHROUGH, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	void onDie(const partest::EventDie &event) override
+	{
+		writeLog(EVENT_DIE, std::make_unique<partest::EventBeginTest>(event));
+	}
+
+	const std::vector<partest::EventPair> &logs() { return m_logs; }
+	void clearLogs() { m_logs.clear(); }
+};
+
+class DispatcherTests : public partest::PartestBase
+{
+	std::unique_ptr<partest::EventDispatcherInterface> m_dispatcher;
+	std::vector<MockReporter> m_reporters;
+	std::vector<partest::EventInterface> m_logs;
+
+	void mirrorLog(PARTEST_STRING_PARAM eventType, std::unique_ptr<partest::EventInterface> event)
+	{
+		m_logs.emplace_back(std::make_pair(eventType, std::move(event)));
+	}
+
+	// Setup to initialize the dispatcher
+	void setUpSerial()
+	{
+		m_dispatcher = partest::make_unique<partest::SerialEventDispatcher>();
+		initializeReporting(2);
+	}
+
+	void tearDownSerial()
+	{
+		clearReporting();
+	}
+
+	void setUpConcurrent()
+	{
+		m_dispatcher = partest::make_unique<partest::ConcurrentEventDispatcher>();
+		initializeReporting(6);
+	}
+
+	void tearDownConcurrent()
+	{
+		clearReporting();
+	}
+
+	void initializeReporting(unsigned reporterCount = 2)
+	{
+		for(unsigned x = 0; x < reporterCount; ++x)
+		{
+			m_reporters.push_back(MockReporter());
+		}
+	}
+	void clearReporting()
+	{
+		m_reporters.clear();
+		m_logs.clear();
+	}
+
+public:
+	DispatcherTests() : PartestBase("DispatcherTests", "Tests for the EventDispatcher classes.")
+	{
+		partest::TestFlags flags = partest::TEST_FLAGS_INHERIT;
+
+		addTest(partest::TestInfo("Test Serial Dispatcher", "Validate that dispatcher passes all events intact to reporters, in sequential order."),
+			flags,
+			[this]() { return this->SerialDispatcherPassesAllEvents(); },
+			[this]() { return this->setUpSerial(); },
+			[this]() { return this->tearDownSerial(); }
+		);
+	}
+
+	// Test serial dispatcher.
+	// Ensure all events are passed to the correct handlers, with original data intact.
+	// Ensure that EVENT_DIE is the last event passed, when killDispatcher is invoked.
+	// Ensure that all reporters record identical events to the reference logs, in identical order.
+	void SerialDispatcherPassesAllEvents()
+	{
+
+	}
+};
+
+#endif
