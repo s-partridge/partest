1035340ba9efe3285ada67a326c8c4f09df53d54
Samuel Partridge <s-partridge@hotmail.com>
2026-07-21 19:39:41 -0700

    Added readonly TestFrameView wrapper for TestFrame

--- include/partest/testframeview.h
+++ include/partest/testframeview.h
@@ -5,6 +5,19 @@
 
 namespace partest
 {
+	class TestFrameView
+	{
+		const TestFrame &m_testFrame;
+
+	public:
+		TestFrameView(const TestFrame &testFrame) : m_testFrame(testFrame) {}
+
+		unsigned id() const noexcept { return m_testFrame.id(); }
+		unsigned parentId() const noexcept { return m_testFrame.parentId(); }
+		const TestInfo &info() const noexcept { return m_testFrame.metadata; }
+		const TestFlags &flags() const noexcept { return m_testFrame.flags; }
+		const TestState &state() const noexcept { return m_testFrame.state; }
+	};
 }
 
 #endif
