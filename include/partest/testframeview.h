c1d2f0e4bf0a66271f6d1a50f9ebb75ff12307da
Samuel Partridge <s-partridge@hotmail.com>
2026-07-21 20:18:31 -0700

    Replaced const refs with const ptrs for new View classes

--- include/partest/testframeview.h
+++ include/partest/testframeview.h
@@ -7,18 +7,18 @@ namespace partest
 {
 	class TestFrameView
 	{
-		const TestFrame &m_testFrame;
+		const TestFrame *m_testFrame;
 
 	public:
-		TestFrameView(const TestFrame &testFrame) : m_testFrame(testFrame) {}
+		TestFrameView(const TestFrame &testFrame) : m_testFrame(&testFrame) {}
 		
 		static TestFrameView getNullTestFrameView() { return TestFrameView(TestFrame::getNullTestFrameInstance()); }
 
-		unsigned id() const noexcept { return m_testFrame.id(); }
-		unsigned parentId() const noexcept { return m_testFrame.parentId(); }
-		const TestInfo &info() const noexcept { return m_testFrame.metadata; }
-		const TestFlags &flags() const noexcept { return m_testFrame.flags; }
-		const TestState &state() const noexcept { return m_testFrame.state; }
+		unsigned id() const noexcept { return m_testFrame->id(); }
+		unsigned parentId() const noexcept { return m_testFrame->parentId(); }
+		const TestInfo &info() const noexcept { return m_testFrame->metadata; }
+		const TestFlags &flags() const noexcept { return m_testFrame->flags; }
+		const TestState &state() const noexcept { return m_testFrame->state; }
 
 	};
 }
