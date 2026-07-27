1590b4958655a21c6b72aeab10ce5b0bcc38f11a
Samuel Partridge <s-partridge@hotmail.com>
2026-07-27 13:55:22 -0700

    Extracted base test validation into TestBaseTests

---
+++ tests/baseTests.h
@@ -0,0 +1,150 @@
+#ifndef BASETESTS_H
+#define BASETESTS_H
+
+#include <partest/testbase.h>
+
+class TestBaseTests : public partest::TestBase
+{
+public:
+	TestBaseTests() : TestBase("TestBaseTests", "Core validation for the Partest base class.")
+	{		
+		// Example of adding a test
+		partest::TestFlags flags = partest::TEST_FLAGS_INHERIT;
+
+		addTest(partest::TestInfo("FailingTest", "A test that always fails."),
+			flags,
+			[this]() { return this->failingTest(); });
+		addTest(partest::TestInfo("MixedTest", "A test that has mixed results."),
+			flags,
+			[this]() { return this->mixedTest(); });
+		addTest(partest::TestInfo("PassedTest", "A test that always passes."),
+			flags,
+			[this]() { return this->passedTest(); });
+		addTest(partest::TestInfo("NestedNestedTest", "A test with nested subtests."),
+			partest::TEST_FLAGS_INHERIT,
+			[this]() { return this->nestedNestedTest(); });
+
+		partest::TestInfo metadata("ParameterizedTest", "Validate that parameters are passed correctly.");
+		flags = partest::TEST_FLAGS_SKIP;
+		addTest(metadata, flags, [this]() { return this->parameterizedTest(3); });
+		addTest(metadata, flags, [this]() { return this->parameterizedTest(6); });
+
+		flags.skip = partest::FlagState::Disabled;
+		flags.stopOnFail = partest::FlagState::Enabled;
+
+		addTest(partest::TestInfo("TestWithStopOnFail", "A test with stopOnFail enabled."),
+			flags,
+			[this]() { return this->testWithStopOnFail(); });
+	}
+
+	void sampleTest()
+	{
+		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "This is a sample log message.");
+		ASSERT_TRUE(true); // This assertion will pass
+	}
+
+	void failingTest()
+	{
+		ASSERT_TRUE(false); // This assertion will fail
+	}
+
+	void mixedTest()
+	{
+		ASSERT_TRUE(true);  // This assertion will pass
+		ASSERT_TRUE(false); // This assertion will fail
+	}
+
+	void passedTest()
+	{
+		ASSERT_TRUE(true); // This assertion will pass
+	}
+
+	void parameterizedTest(int testValue)
+	{
+		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Running example test...");
+		subtest(partest::TestInfo("Subtest1", "A subtest that checks if testValue is 3."), [&]()
+		{
+			// Subtest logic here
+			ASSERT_TRUE(testValue == 3);
+		});
+
+		subtest(partest::TestInfo("Subtest2", "A subtest that checks if testValue is 6."), [&]()
+		{
+			// Subtest logic here
+			ASSERT_TRUE(testValue == 6);
+		});
+	}
+
+	// This test should result in a Mixed status due to nested subtests
+	void nestedNestedTest()
+	{
+		subtest(partest::TestInfo("NestedSubtest1", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
+		{
+			ASSERT_TRUE(true); // This assertion will pass
+			
+			subtest(partest::TestInfo("NestedSubtest 1.1"), [this]()
+			{
+				ASSERT_TRUE(false); // This assertion will fail
+			});
+
+			subtest(partest::TestInfo("NestedSubtest1.2", "A nested subtest that always fails."), partest::TEST_FLAGS_INHERIT, [this]()
+			{
+				ASSERT_TRUE(false); // This assertion will fail
+			});
+
+			subtest(partest::TestInfo("NestedSubtest1.3", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
+			{
+				ASSERT_TRUE(true); // This assertion will pass
+			});
+		});
+
+		subtest(partest::TestInfo("NestedSubtest2", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
+		{
+			ASSERT_TRUE(true); // This assertion will pass
+		});
+	}
+
+	void testWithStopOnFail()
+	{
+		partest::TestFlags stopFlags = partest::TEST_FLAGS_INHERIT;
+		stopFlags.stopOnFail = partest::FlagState::Enabled;
+
+		subtest(partest::TestInfo("Subtest1", "A subtest that checks if 1 + 1 == 2."), stopFlags, [&]()
+		{
+			// This assertion will pass
+			ASSERT_TRUE(1 + 1 == 2);
+		});
+
+		subtest(partest::TestInfo("Subtest2", "A subtest that checks if 2 + 2 == 5."), stopFlags, [&]()
+		{
+			subtest(partest::TestInfo("NestedSubtest", "A nested subtest that checks if 2 + 2 == 4."), stopFlags, [&]()
+			{
+				// This assertion will fail
+				ASSERT_TRUE(2 + 2 == 5);
+			});
+
+			// This assertion will pass, if it is hit, which it shouldn't be if stopOnFail is Enabled
+			std::cout << "Error: This assertion should not run if stopOnFail is ENABLED and a previous assertion failed." << std::endl;
+			ASSERT_TRUE(2 + 2 == 4);
+		});
+
+		subtest(partest::TestInfo("Subtest3", "A subtest that checks if 3 + 3 == 6."), stopFlags, [&]()
+		{
+			std::cout << "Error: This subtest should not run if stopOnFail is ENABLED and a previous assertion failed." << std::endl;
+			// This assertion will pass, but may not be reached if stopOnFail is Enabled in Subtest2
+			ASSERT_TRUE(3 + 3 == 6);
+		});
+	}
+
+	void setup() override
+	{
+		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Setting up PartestBaseTest...");
+	}
+
+	void teardown() override
+	{
+		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Tearing down PartestBaseTest...");
+	}
+};
+
+#endif
--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include <partest/bootstrap.h>
+#include "baseTests.h"
 #include "assertionTests.h"
 #include "semaphoreTests.h"
 #include "dispatcherTests.h"
@@ -14,150 +15,6 @@
 	#include <crtdbg.h>
 #endif
 
-class PartestBaseTest : public partest::TestBase
-{
-public:
-	PartestBaseTest() : TestBase("PartestBaseTest", "Core validation for the Partest base class.")
-	{		
-		// Example of adding a test
-		partest::TestFlags flags = partest::TEST_FLAGS_INHERIT;
-
-		addTest(partest::TestInfo("FailingTest", "A test that always fails."),
-			flags,
-			[this]() { return this->failingTest(); });
-		addTest(partest::TestInfo("MixedTest", "A test that has mixed results."),
-			flags,
-			[this]() { return this->mixedTest(); });
-		addTest(partest::TestInfo("PassedTest", "A test that always passes."),
-			flags,
-			[this]() { return this->passedTest(); });
-		addTest(partest::TestInfo("NestedNestedTest", "A test with nested subtests."),
-			partest::TEST_FLAGS_INHERIT,
-			[this]() { return this->nestedNestedTest(); });
-
-		partest::TestInfo metadata("ParameterizedTest", "Validate that parameters are passed correctly.");
-		flags = partest::TEST_FLAGS_SKIP;
-		addTest(metadata, flags, [this]() { return this->parameterizedTest(3); });
-		addTest(metadata, flags, [this]() { return this->parameterizedTest(6); });
-
-		flags.skip = partest::FlagState::Disabled;
-		flags.stopOnFail = partest::FlagState::Enabled;
-
-		addTest(partest::TestInfo("TestWithStopOnFail", "A test with stopOnFail enabled."),
-			flags,
-			[this]() { return this->testWithStopOnFail(); });
-	}
-
-	void sampleTest()
-	{
-		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "This is a sample log message.");
-		ASSERT_TRUE(true); // This assertion will pass
-	}
-
-	void failingTest()
-	{
-		ASSERT_TRUE(false); // This assertion will fail
-	}
-
-	void mixedTest()
-	{
-		ASSERT_TRUE(true);  // This assertion will pass
-		ASSERT_TRUE(false); // This assertion will fail
-	}
-
-	void passedTest()
-	{
-		ASSERT_TRUE(true); // This assertion will pass
-	}
-
-	void parameterizedTest(int testValue)
-	{
-		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Running example test...");
-		subtest(partest::TestInfo("Subtest1", "A subtest that checks if testValue is 3."), [&]()
-		{
-			// Subtest logic here
-			ASSERT_TRUE(testValue == 3);
-		});
-
-		subtest(partest::TestInfo("Subtest2", "A subtest that checks if testValue is 6."), [&]()
-		{
-			// Subtest logic here
-			ASSERT_TRUE(testValue == 6);
-		});
-	}
-
-	// This test should result in a Mixed status due to nested subtests
-	void nestedNestedTest()
-	{
-		subtest(partest::TestInfo("NestedSubtest1", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
-		{
-			ASSERT_TRUE(true); // This assertion will pass
-			
-			subtest(partest::TestInfo("NestedSubtest 1.1"), [this]()
-			{
-				ASSERT_TRUE(false); // This assertion will fail
-			});
-
-			subtest(partest::TestInfo("NestedSubtest1.2", "A nested subtest that always fails."), partest::TEST_FLAGS_INHERIT, [this]()
-			{
-				ASSERT_TRUE(false); // This assertion will fail
-			});
-
-			subtest(partest::TestInfo("NestedSubtest1.3", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
-			{
-				ASSERT_TRUE(true); // This assertion will pass
-			});
-		});
-
-		subtest(partest::TestInfo("NestedSubtest2", "A nested subtest that always passes."), partest::TEST_FLAGS_INHERIT, [this]()
-		{
-			ASSERT_TRUE(true); // This assertion will pass
-		});
-	}
-
-	void testWithStopOnFail()
-	{
-		partest::TestFlags stopFlags = partest::TEST_FLAGS_INHERIT;
-		stopFlags.stopOnFail = partest::FlagState::Enabled;
-
-		subtest(partest::TestInfo("Subtest1", "A subtest that checks if 1 + 1 == 2."), stopFlags, [&]()
-		{
-			// This assertion will pass
-			ASSERT_TRUE(1 + 1 == 2);
-		});
-
-		subtest(partest::TestInfo("Subtest2", "A subtest that checks if 2 + 2 == 5."), stopFlags, [&]()
-		{
-			subtest(partest::TestInfo("NestedSubtest", "A nested subtest that checks if 2 + 2 == 4."), stopFlags, [&]()
-			{
-				// This assertion will fail
-				ASSERT_TRUE(2 + 2 == 5);
-			});
-
-			// This assertion will pass, if it is hit, which it shouldn't be if stopOnFail is Enabled
-			std::cout << "Error: This assertion should not run if stopOnFail is ENABLED and a previous assertion failed." << std::endl;
-			ASSERT_TRUE(2 + 2 == 4);
-		});
-
-		subtest(partest::TestInfo("Subtest3", "A subtest that checks if 3 + 3 == 6."), stopFlags, [&]()
-		{
-			std::cout << "Error: This subtest should not run if stopOnFail is ENABLED and a previous assertion failed." << std::endl;
-			// This assertion will pass, but may not be reached if stopOnFail is Enabled in Subtest2
-			ASSERT_TRUE(3 + 3 == 6);
-		});
-	}
-
-	void setup() override
-	{
-		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Setting up PartestBaseTest...");
-	}
-
-	void teardown() override
-	{
-		recordLog(partest::LogLevel::Debug, partest::LOG_TYPE_TEST, "Tearing down PartestBaseTest...");
-	}
-};
-
 int main(int argc, const char **argv)
 {
 // For memory leak validation on MSVC
@@ -165,8 +22,8 @@ int main(int argc, const char **argv)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 	partest::initializeSuite(argc, argv);
-	//partest::addTestClass(partest::make_unique<PartestBaseTest>());
-	//partest::addTestClass(partest::make_unique<AssertionTests>());
+	partest::addTestClass(partest::make_unique<TestBaseTests>());
+	partest::addTestClass(partest::make_unique<AssertionTests>());
 	partest::addTestClass(partest::make_unique<SemaphoreTests>());
 	partest::addTestClass(partest::make_unique<DispatcherTests>());
 	partest::addTestClass(partest::make_unique<XMLNodeTests>());
